54ecde0347386ebe780b52b2f537224643073c68
Chymera92 <hemanth.bm12@gmail.com>
2017-09-13 16:36:20 -0700

    reversePosIndex using concurrent map! read docs..(IMP!!)

--- src/search/reversePosIndex.java
+++ src/search/reversePosIndex.java
@@ -1,11 +1,18 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by chimera on 9/11/17.
  */
 
-//
-//public class reversePosIndex {
-//    private
-//
-//
-//
-//}
+
+public class reversePosIndex {
+    private ConcurrentHashMap universalIndex;
+    public reversePosIndex()
+    {
+        universalIndex=new ConcurrentHashMap<String,ConcurrentHashMap<String,ArrayList>>();
+    }
+}
